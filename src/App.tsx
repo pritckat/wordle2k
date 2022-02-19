30fe154f9c37c1e1c366f96f5c5d19908ba1ca86
pritckat <pritckat@gmail.com>
2022-02-19 17:44:52 -0500

    add word row ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from 'react';
+import WordRow from './WordRow';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="mx-auto w-96">
-      <h1 className="text-4xl text-center">Wordle 2k</h1>
+      <header className='border-b border-gray-500 pb-2 mb-2 my-2'>
+        <h1 className="text-4xl text-center">Wordle 2k</h1>
+      </header>
+
+      <main>
+        <WordRow letters="hel"/>
+        <WordRow letters="hello" />
+      </main>
     </div>
   )
 }
---
+++ src/WordRow.tsx
@@ -0,0 +1,30 @@
+const LETTER_LENGTH = 5;
+
+interface WordRowProps {
+    letters: string;
+}
+
+export default function WordRow({letters: lettersProp = '' }: WordRowProps) {
+    const lettersRemaining = LETTER_LENGTH - lettersProp.length
+    const letters = lettersProp
+        .split('')
+        .concat(Array(lettersRemaining).fill(''))
+    return(
+    <div className="grid grid-cols-5 gap-4 text-center">
+        {letters.map((char) => (
+            <CharacterBox key={char} value={char} />
+        ))}
+    </div>
+    )
+}
+interface CharacterBoxProps {
+    value: string;
+}
+
+function CharacterBox({value}: CharacterBoxProps) {
+    return (
+        <div className="inline-block border-2 border-gray-500 p-4 uppercase font-bold text-2xl">
+            {value}
+        </div>
+    )
+}
